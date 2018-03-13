f3badc5a90a14992b8faa4ae50d60d3d5923818b
Florence <florence.lo@hotmail.com>
2018-03-14 03:01:16 +1300

    update

--- js/flo.js
+++ js/flo.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+getConfig();
+var apiKey;
+
+function getConfig() {
+	    $.ajax({
+            url: "../config.json",
+            dataType: 'json',
+            success: function(configData){
+                apiKey = configData[0].apiKey;
+               
+                runApp();
+                
+            },
+            error: function(error){
+                console.log(error);
+                console.log("Something has gone wrong");
+            }
+    });
+}
+
+function runApp() {
+    console.log(apiKey);
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,71 @@
 console.log('hello carey');
+
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawDashboard);
+
+// var apiKey;
+// var designerIdOne;
+// var designerIdTwo;
+// var designerIdThree;
+// var designerIdFour;
+// var designerIdFive;
+// var designerIdSix;
+
+// function getConfig(){
+// 	    $.ajax({
+//         url: "../config.json",
+//         dataType: 'json',
+//         success: function(configData){
+//            apiKey = configData[0].apiKey;
+//            console.log(apiKey);
+         
+//             // getBehance();
+//         },
+//         error: function(error){
+//             console.log(error);
+//             console.log("Something has gone wrong");
+//         }
+//     })
+// }
+
+// // function drawDashboard(){
+// // 	console.log(apiKey);
+// // 	    $.ajax({
+// //         url: "https://api.behance.net/v2/users/matiascorea/projects?client_id=1234567890",
+// //         dataType: 'jsonP',
+// //         success: function(data){
+// //           //where the function to create the charts wiill go
+// //         },
+// //         error: function(error){
+// //             console.log(error);
+// //             console.log("Something has gone wrong");
+// //         }
+// //     })
+// // }
+
 console.log('hello kelsey');
-console.log('hello world');
+
+
+getConfig();
+var apiKey;
+
+function getConfig() {
+	    $.ajax({
+            url: "../config.json",
+            dataType: 'json',
+            success: function(configData){
+                apiKey = configData[0].apiKey;
+               
+                runApp();
+                
+            },
+            error: function(error){
+                console.log(error);
+                console.log("Something has gone wrong");
+            }
+    });
+}
+
+function runApp() {
+    console.log(apiKey);
+}
