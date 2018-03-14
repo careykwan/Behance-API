6e19656fdd1aa9c165bf1196ebc13c6a436a6e9f
Kelseys1993 <kelsey.j.smith18@gmail.com>
2018-03-14 16:04:22 +1300

    added graph

--- js/kelsey.js
+++ js/kelsey.js
@@ -1,25 +1,49 @@
-
-// google.charts.load('current', {'packages':['corechart']});
-// google.charts.setOnLoadCallback(drawDashboard);
-
-var apiKey;
-var desingerId = ['njenworks', 'RomaNaim', "stefanocolferai"];
-var url1 = "https://api.behance.net/v2/users/njenworks/projects?client_id=s2I0yUtgNQA70LjwEMBJfy3TWu2MGOsG";
-var url2 = "https://api.behance.net/v2/users/RomanNaim/projects?client_id=s2I0yUtgNQA70LjwEMBJfy3TWu2MGOsG";
-// var designerIdTwo;
-// var designerIdThree;
-// var designerIdFour;
-// var designerIdFive;
-// var designerIdSix;
+  
+google.charts.load('current', {'packages':['corechart' ,'bar']});
+ google.charts.setOnLoadCallback(getApiData);
 
 
-function drawDashboard(){
+var apiKey = 's2I0yUtgNQA70LjwEMBJfy3TWu2MGOsG';
+var designerId = 'njenworks';
+
+
+
+function getApiData(){
 	    $.ajax({     
-        url: url1, 
+        url:'https://api.behance.net/v2/users/' + designerId + '/projects?client_id=' + apiKey,
         dataType: 'jsonP',
-        success: function(data){
-            console.log(data);
-          //where the function to create the charts will go
+        success: function(dataFromJSONP){
+
+  
+    console.log(dataFromJSONP);
+    console.log(dataFromJSONP.projects[0].stats.appreciations);
+    
+    var data = new google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['appreciations', dataFromJSONP.projects[0].stats.appreciations ],
+        ['views',  dataFromJSONP.projects[0].stats.views],
+        ['appreciations', dataFromJSONP.projects[1].stats.appreciations ],
+        ['views',  dataFromJSONP.projects[1].stats.views],
+        ['appreciations', dataFromJSONP.projects[2].stats.appreciations ],
+        ['views',  dataFromJSONP.projects[2].stats.views]
+    
+   ]);
+
+   
+       var options = {
+          chart: {
+            title: 'Appreciations vs Views',
+          },
+          bars: 'vertical',
+          vAxis: {format: 'decimal'},
+          height: 400,
+          colors: ['#1b9e77', '#d95f02', '#7570b3']
+        };
+ 
+   var classicChart = new google.charts.Bar(document.getElementById('chart1'));
+    classicChart.draw(data, google.charts.Bar.convertOptions(options) );
+       
+              
 
         },
         error: function(error){
@@ -28,6 +52,6 @@ function drawDashboard(){
         }
     });
 }
-drawDashboard();
+
 console.log('hello kelsey');
 
