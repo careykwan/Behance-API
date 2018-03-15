c91f8cc7d10db9e505c56b2f816aca9f10b62346
Kelseys1993 <kelsey.j.smith18@gmail.com>
2018-03-15 13:07:04 +1300

    adding more js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 console.log('hello carey');
-  
+
 google.charts.load('current', {'packages':['corechart' ,'bar']});
- google.charts.setOnLoadCallback(getApiData);
+google.charts.setOnLoadCallback(getApiData);
 
 
 var apiKey = 's2I0yUtgNQA70LjwEMBJfy3TWu2MGOsG';
-var designerId = 'njenworks';
-
-
+var designerId = 'clairehartley';
+var designer1 = 'angushyland';
+var designer2 = 'clairehartley';
+var designer3 = 'Doralice';
+var designer4 = 'izutsu';
+var designer5 = 'MaryRabun';
+var designer6 = 'nahuelsal';
 
 function getApiData(){
 	    $.ajax({     
         url:'https://api.behance.net/v2/users/' + designerId + '/projects?client_id=' + apiKey,
         dataType: 'jsonP',
         success: function(dataFromJSONP){
+        var totalAppreciations =  dataFromJSONP.projects[0].stats.appreciations + 
+        dataFromJSONP.projects[1].stats.appreciations + 
+        dataFromJSONP.projects[2].stats.appreciations;
+        var totalViews = dataFromJSONP.projects[0].stats.views + 
+        dataFromJSONP.projects[1].stats.views +
+        dataFromJSONP.projects[2].stats.views;
+        var totalComments = dataFromJSONP.projects[0].stats.comments +
+        dataFromJSONP.projects[1].stats.comments  +
+        dataFromJSONP.projects[2].stats.comments;
+        var designerName = dataFromJSONP.projects[0].owners[0].display_name;
 
-  
+
+  console.log(totalViews);
     console.log(dataFromJSONP);
     console.log(dataFromJSONP.projects[0].stats.appreciations);
     
@@ -44,17 +59,22 @@ function getApiData(){
           },
         
           fontName: 'Playfair Display, serif',
+          fontSize: 14,
           bars: 'vertical',
-          vAxis: {format: 'decimal'},
-          height: 300,
-          width:700,
+          vAxis: {
+            format: 'decimal',
+            },
           colors: ['#1B2D47', '#1E3E75']
         };
  
    var classicChart = new google.charts.Bar(document.getElementById('chart1Location'));
     classicChart.draw(data, options);
        
-              
+       // $('#designerName3').append(designerName);
+       // $('#viewStats3').append(totalViews);
+       // $('#apreciationStats3').append(totalAppreciations);
+       // $('#commentStats3').append(totalComments);
+            
 
         },
         error: function(error){
@@ -63,7 +83,9 @@ function getApiData(){
         }
     });
 }
-
+  $(window).resize(function(){
+        getApiData();
+        }) ;
 console.log('hello kelsey');
 
 
